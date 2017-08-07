5798c5cc65b529ece5d6884d7684018971e8173b
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2017-08-07 15:19:06 +0700

    tao input(type="hidden" name="_token" value="{{csrf_token}}" ) in <form> to sent data to database

--- resources/views/admin/theloai/them.blade.php
+++ resources/views/admin/theloai/them.blade.php
@@ -1,57 +1,30 @@
 @extends('admin.layout.index')
 @section('content')
-<!-- Page Content -->
-<div id="page-wrapper">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">Thể Loại
-                    <small>Thêm</small>
-                </h1>
+    <!-- Page Content -->
+    <div id="page-wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Thể Loại
+                        <small>Thêm</small>
+                    </h1>
+                </div>
+                <!-- /.col-lg-12 -->
+                <div class="col-lg-7" style="padding-bottom:120px">
+                    <form action="admin/theloai/them" method="POST">
+                        <div class="form-group">
+                            <input type="hidden" name="_token" value="{{csrf_token()}}">
+                            <label>Category Name</label>
+                            <input class="form-control" name="Ten" placeholder="Please Enter Category Name"/>
+                        </div>
+                        <button type="submit" class="btn btn-default">Category Add</button>
+                        <button type="reset" class="btn btn-default">Reset</button>
+                        </form>
+                </div>
             </div>
-            <!-- /.col-lg-12 -->
-            <div class="col-lg-7" style="padding-bottom:120px">
-                <form action="" method="POST">
-                    <div class="form-group">
-                        <label>Category Parent</label>
-                        <select class="form-control">
-                            <option value="0">Please Choose Category</option>
-                            <option value="">Tin Tức</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label>Category Name</label>
-                        <input class="form-control" name="txtCateName" placeholder="Please Enter Category Name" />
-                    </div>
-                    <div class="form-group">
-                        <label>Category Order</label>
-                        <input class="form-control" name="txtOrder" placeholder="Please Enter Category Order" />
-                    </div>
-                    <div class="form-group">
-                        <label>Category Keywords</label>
-                        <input class="form-control" name="txtOrder" placeholder="Please Enter Category Keywords" />
-                    </div>
-                    <div class="form-group">
-                        <label>Category Description</label>
-                        <textarea class="form-control" rows="3"></textarea>
-                    </div>
-                    <div class="form-group">
-                        <label>Category Status</label>
-                        <label class="radio-inline">
-                            <input name="rdoStatus" value="1" checked="" type="radio">Visible
-                        </label>
-                        <label class="radio-inline">
-                            <input name="rdoStatus" value="2" type="radio">Invisible
-                        </label>
-                    </div>
-                    <button type="submit" class="btn btn-default">Category Add</button>
-                    <button type="reset" class="btn btn-default">Reset</button>
-                    <form>
-            </div>
+            <!-- /.row -->
         </div>
-        <!-- /.row -->
+        <!-- /.container-fluid -->
     </div>
-    <!-- /.container-fluid -->
-</div>
-<!-- /#page-wrapper -->
-    @endsection
+    <!-- /#page-wrapper -->
+@endsection
